eddd6020e13d179080376109f6b2b464c33fb82b
SergeyKrichevsky <ISIFway@gmail.com>
2025-05-11 15:20:58 +0300

    15:20

---
+++ W1/D1/DailyChallenge/Challenge_w1_d1.py
@@ -0,0 +1,19 @@
+user_string = input('Enter a string 10 characters long ')
+if len(user_string) < 10:
+    print('String not long enough.')
+elif len(user_string) > 10:
+    print('String too long.')
+else:
+    print('Perfect string')
+
+print("First character:",  user_string[0])
+print("Last character:", user_string[-1])
+
+for i in range(1, len(user_string) + 1):
+    print(user_string[:i])
+
+import random
+char_list = list(user_string)
+random.shuffle(char_list)
+jumbled_string = ''.join(char_list)
+print('jumbled_string:', jumbled_string)
